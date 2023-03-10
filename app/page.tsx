ca44843c0d8b0f70998bbb34e7c97726f04744c1
franktunde <adeekotobiloba8@gmail.com>
2023-03-10 01:14:37 +0100

    chore(setup): setting up nextjs and tailwind

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1 className='text-4xl' >Welcome to the Blog</h1>
+        <p>Some content</p>
+    </div>
+  )
+}
+
+export default HomePage;
